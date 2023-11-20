e9838adcc3cb92e725e5ecc831e0aeb50b843c9b
Daniel Riveros <driveros@micredi.com.co>
2023-11-20 15:18:13 -0500

    1. Roles are added for the identity server 2. Identity server database migrations are added

--- TaskManagement.identity/Configurations/UserRoleConfiguration.cs
+++ TaskManagement.identity/Configurations/UserRoleConfiguration.cs
@@ -11,13 +11,13 @@ namespace TaskManagement.Identity.Configurations
             builder.HasData(
                 new IdentityUserRole<string>
                 {
-                    RoleId = "",
-                    UserId = ""
+                    RoleId = "ad56b66f-d40a-4980-bb1e-3236b3c221eb",
+                    UserId = "710ddf9a-b3f9-418a-a8ec-bfc2742427f7"
                 },
                 new IdentityUserRole<string>
                 {
-                    RoleId = "",
-                    UserId = ""
+                    RoleId = "2c615cff-3d83-4bb7-a0c3-8d95d3ad696c",
+                    UserId = "832700a3-1ddc-40b8-a7fc-c15902ecd159"
                 })
                 ;
         }
