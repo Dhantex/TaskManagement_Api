a7065fb1fa8c779bcaf6fe8e394d6ed4f29e1095
Daniel Riveros <driveros@micredi.com.co>
2023-11-20 14:17:55 -0500

    Service registration is done for the TaskManagerDbContext

---
+++ TaskManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TaskManagement.Application.Contracts.Persistence;
+using TaskManagement.Infrastructure.Persistence;
+using TaskManagement.Infrastructure.Repositories;
+
+namespace TaskManagement.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<TaskManagerDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            return services;
+        }
+    }
+}
